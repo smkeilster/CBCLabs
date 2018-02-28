91fe53ae43559a209a9bbcaa0de98dc2c3d25f4f
smkeilster <keil.stephanie.m@gmail.com>
2018-02-28 14:47:35 -0500

    2.28 To Do list completed lab

--- 2.27lab/app/components/Grid/index.js
+++ 2.27lab/app/components/Grid/index.js
@@ -9,29 +9,29 @@ import React from 'react';
 import './style.css';
 import './styleM.css';
 
-export default class Grid extends React.PureComponent 
+export default class Grid extends React.PureComponent
 {
-  constructor () 
+  constructor ()
   {
     super();
-    this.state = 
+    this.state =
     {
       images: ['Hiking1.jpg','Hiking2.jpg','Hiking3.jpg','Hiking4.jpg','Hiking5.jpg','Hiking6.jpg','Hiking7.jpg','Hiking8.jpg']
     }
-    
+
   }
-  
-  
+
+
   render() {
     return (
       <div>
         <div className="grid">
         {this.state.images.map((image, index) =>(
-        <div className="gridItem" key={index}>
+        <div className="gridItem" key={'gridItem${index}'}>
           <img src={require('../../images/'+image)} className="gridImage"/>
-        </div>  
+        </div>
         ))}
-        </div>  
+        </div>
       </div>
     );
   }
---
+++ 2.28ToDo/app/containers/Home/index.js
@@ -0,0 +1,66 @@
+/*
+ *
+ * Home
+ *
+ */
+
+import React from 'react';
+import Helmet from 'react-helmet';
+
+import './style.css';
+import './styleM.css';
+
+export default class Home extends React.PureComponent {
+  constructor()
+  {
+    super();
+    this.state = {
+      listItems: [],
+      inputItem:""
+    }
+  };
+
+  handleItem = (event) =>
+  {
+    this.setState({
+      inputItem:event.target.value
+    })
+  };
+
+  storeItem = () =>
+  {
+    var listItems = this.state.listItems;
+    var inputItem = this.state.inputItem;
+
+    if(inputItem !== "") {
+        listItems.push(inputItem);
+
+        this.setState({
+          listItems:listItems,
+          inputItem: ""
+        })
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
+
+        <div className = "inputContainer">
+          <input type="text" className="todoInput" onChange={this.handleItem} value={this.state.inputItem}/>
+          <input type="submit" value="Add to List" className="todoButton" onClick={this.storeItem}/>
+        </div>
+
+        <div className="todoList">
+          {this.state.listItems.map((item, index) => (
+            <div className="listItem" key={index}>
+              {item}
+            </div>
+          ))}
+          </div>
+
+      </div>
+    );
+  }
+}
