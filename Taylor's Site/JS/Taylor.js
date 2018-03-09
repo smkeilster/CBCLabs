9e4bb749e57109743944de76217edccbf5079c36
smkeilster <keil.stephanie.m@gmail.com>
2018-03-09 13:18:03 -0500

    fixing repo and adding 3.8 and 3.9

--- Taylor's Site/JS/Taylor.js
+++ Taylor's Site/JS/Taylor.js
@@ -1,9 +1,5 @@
 // Get the modal
-<<<<<<< HEAD
-let modal = document.getElementById('signup');
-=======
 let modal = document.getElementById('id01');
->>>>>>> 748d9c6633a831929e6ea9a3d6bad6fef58fe70d
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
